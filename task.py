3eeb5fbdee0277f633bf40d61dd29e7ef861602e
Maksim Rybakov <m.p.rybakov@gmail.com>
2025-11-03 22:06:23 +0300

    improve task logs

--- task.py
+++ task.py
@@ -1,8 +1,9 @@
 import random
 import time
+from datetime import datetime
 
 
 def run_task(task):
     execution_time = random.randint(0,10)
-    print(f"Running task {task.id} for {execution_time}")
+    print(f"{datetime.now()} Running task {task.id} for {execution_time}")
     time.sleep(execution_time)
